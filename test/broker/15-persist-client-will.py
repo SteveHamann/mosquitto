CodeLinaro-mirror/la_platform_external_eclipse_mosquitto

Tests: Use tempfile for broker logging to prevent stderr buffer filling

test/broker/15-persist-client-will.py
@@ -175,7 +175,7 @@ def do_test(
 
         # Kill the broker
         broker.kill()
-        (_, stde) = broker.communicate()
+        stde = mosq_test.broker_log(broker)
         broker = None
 
         if will_sent and will_qos > 0:
@@ -225,7 +225,7 @@ def do_test(
             if mosq_test.wait_for_subprocess(broker):
                 if rc == 0:
                     rc = 1
-            (_, stde3) = broker.communicate()
+            stde3 = mosq_test.broker_logs(broker)
             if not stde:
                 stde = stde3
             else:
@@ -235,10 +235,10 @@ def do_test(
 
         if rc:
             if stde:
-                print(stde.decode("utf-8"))
+                print(stde)
             if stde2:
                 print("Broker after restart")
-                print(stde2.decode("utf-8"))
+                print(stde2)
         # assert rc == 0, f"rc: {rc}"
 
 

test/broker/22-http-api-acl.py
@@ -62,9 +62,8 @@ finally:
     if mosq_test.wait_for_subprocess(broker):
         print("broker not terminated")
         if rc == 0: rc=1
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_logs(broker))
         rc = 1
 
 

test/broker/22-http-api-api.py
@@ -193,9 +193,8 @@ finally:
         os.remove(f"{mqtt_port}.sock")
     except FileNotFoundError:
         pass
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_logs(broker))
         rc = 1
 
 

test/broker/22-http-api-auth.py
@@ -73,9 +73,8 @@ finally:
     if mosq_test.wait_for_subprocess(broker):
         print("broker not terminated")
         if rc == 0: rc=1
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_logs(broker))
         rc = 1
 
 

SteveHamann/mosquitto

test/broker/15-persist-client-will.py
@@ -175,7 +175,7 @@ def do_test(
 
         # Kill the broker
         broker.kill()
-        (_, stde) = broker.communicate()
+        stde = mosq_test.broker_log(broker)
         broker = None
 
         if will_sent and will_qos > 0:
@@ -225,7 +225,7 @@ def do_test(
             if mosq_test.wait_for_subprocess(broker):
                 if rc == 0:
                     rc = 1
-            (_, stde3) = broker.communicate()
+            stde3 = mosq_test.broker_log(broker)
             if not stde:
                 stde = stde3
             else:
@@ -235,10 +235,10 @@ def do_test(
 
         if rc:
             if stde:
-                print(stde.decode("utf-8"))
+                print(stde)
             if stde2:
                 print("Broker after restart")
-                print(stde2.decode("utf-8"))
+                print(stde2)
         # assert rc == 0, f"rc: {rc}"
 
 

test/broker/22-http-api-acl.py
@@ -62,9 +62,8 @@ finally:
     if mosq_test.wait_for_subprocess(broker):
         print("broker not terminated")
         if rc == 0: rc=1
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_log(broker))
         rc = 1
 
 

test/broker/22-http-api-api.py
@@ -193,9 +193,8 @@ finally:
         os.remove(f"{mqtt_port}.sock")
     except FileNotFoundError:
         pass
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_log(broker))
         rc = 1
 
 

test/broker/22-http-api-auth.py
@@ -73,9 +73,8 @@ finally:
     if mosq_test.wait_for_subprocess(broker):
         print("broker not terminated")
         if rc == 0: rc=1
-    (stdo, stde) = broker.communicate()
     if rc != 0:
-        print(stde.decode('utf-8'))
+        print(mosq_test.broker_log(broker))
         rc = 1
 
 